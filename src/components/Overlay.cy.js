c356114f866ea0e0da33c59027d06f4ad81d5698
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-06-17 15:43:41 -0400

    overlay video

--- src/components/Overlay.cy.js
+++ src/components/Overlay.cy.js
@@ -4,20 +4,14 @@ import { Overlay } from './Overlay'
 import '../App.css'
 
 describe('Overlay', () => {
-  it('is invisible and cannot be clicked', () => {
-    cy.mount(<Overlay onClickOverlay={cy.stub().as('click')} />)
+  it('is invisible', () => {
+    cy.mount(<Overlay />)
     cy.get('.overlay').should('not.be.visible')
-    // the overlay cannot be clicked using the cy.click() command
-    // because it is not visible
-    // workaround:
-    // 1. click({ force: true })
-    // 2. invoke('click') // jQuery method
-    // cy.get('@click').should('have.been.calledOnce')
   })
 
   it('is visible and clickable', () => {
     cy.mount(<Overlay overlay={true} onClickOverlay={cy.stub().as('click')} />)
     cy.get('.overlay').should('be.visible').click()
-    cy.get('@click').should('have.been.calledOnce')
+    cy.get('@click').should('have.been.called')
   })
 })
