71d5313b0383c0900fc7fea603ad0d740bee386b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-06-22 21:28:49 -0400

    add stubbing import

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress')
+const webpack = require('webpack')
 
 module.exports = defineConfig({
   e2e: {
@@ -12,6 +13,30 @@ module.exports = defineConfig({
     devServer: {
       framework: 'create-react-app',
       bundler: 'webpack',
+      webpackConfig: {
+        mode: 'development',
+        devtool: false,
+        module: {
+          rules: [
+            {
+              test: /\.?js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react'],
+                  plugins: [
+                    [
+                      '@babel/plugin-transform-modules-commonjs',
+                      { loose: true },
+                    ],
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
     },
     viewportHeight: 1000,
     viewportWidth: 1000,
