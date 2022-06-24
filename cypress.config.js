644646343bef8f7421d88f226762f50bec9ad63e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-06-24 14:50:22 -0400

    add watch and reload plugin

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,13 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    'cypress-watch-and-reload': {
+      watch: ['src/**/*'],
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // https://github.com/bahmutov/cypress-watch-and-reload
+      return require('cypress-watch-and-reload/plugins')(on, config)
     },
   },
 
